28b0656ebc8d3df7cf555fd2c71f516637c9585d
Anup Shahi <bhanushahi70@gmail.com>
2025-08-19 13:05:22 +0530

    Update profile.js

--- lost and found system/project/profile.js
+++ lost and found system/project/profile.js
@@ -1,12 +1,6 @@
 // Firebase Config
-const firebaseConfig = {
-  apiKey: "AIzaSyAsHXV79dwe-KFmEtU9G8_QMjC9eq_zDzY",
-  authDomain: "lost-and-found-system-fc07.firebaseapp.com",
-  projectId: "lost-and-found-system-fc07",
-  storageBucket: "lost-and-found-system-fc07",
-  messagingSenderId: "38915289446",
-  appId: "1:38915289446:web:7b5248b3cdd722d66218a7"
-};
+import firebaseConfig from "./firebaseConfig.js";
+
 firebase.initializeApp(firebaseConfig);
 
 let currentUserId = null;
@@ -136,4 +130,5 @@ function formatDate(dateStr) {
     day: 'numeric'
   });
 }
+
 
