b6d5ebcc4204e2e17cdc769868c2d7e1e7260091
Anup Shahi <bhanushahi70@gmail.com>
2025-08-19 15:12:54 +0530

    Delete lost and found system directory
    deleted my code

--- lost and found system/project/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAsHXV79dwe-KFmEtU9G8_QMjC9eq_zDzY",
-  authDomain: "lost-and-found-system-fc07.firebaseapp.com",
-  projectId: "lost-and-found-system-fc07",
-  storageBucket: "lost-and-found-system-fc07.firebasestorage.app",
-  messagingSenderId: "38915289446",
-  appId: "1:38915289446:web:7b5248b3cdd722d66218a7"
-};
