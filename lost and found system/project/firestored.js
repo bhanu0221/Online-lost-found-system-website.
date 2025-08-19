9b67d610d7dc616422fcb8664ecbc0c0d82e6761
Anup Shahi <bhanushahi70@gmail.com>
2025-08-19 13:09:25 +0530

    Update firestored.js

--- lost and found system/project/firestored.js
+++ lost and found system/project/firestored.js
@@ -1,13 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Firebase configuration
-    const firebaseConfig = {
-        apiKey: "AIzaSyAsHXV79dwe-KFmEtU9G8_QMjC9eq_zDzY",
-        authDomain: "lost-and-found-system-fc07.firebaseapp.com",
-        projectId: "lost-and-found-system-fc07",
-        storageBucket: "lost-and-found-system-fc07.firebasestorage.app",
-        messagingSenderId: "38915289446",
-        appId: "1:38915289446:web:7b5248b3cdd722d66218a7"
-      };
+    import firebaseConfig from "./firebaseConfig.js";
 
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
@@ -62,4 +55,5 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         });
     }
+
 });
